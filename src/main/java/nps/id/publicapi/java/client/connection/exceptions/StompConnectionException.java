87a5dcbd62b2c4ce14b95cb2b404b839f017544e
Pawel Szymus <pszymus@euronext.com>
2024-09-17 17:30:42 +0100

    DEV-36065: New PublicApi Java sample - remove redundant exception type

--- src/main/java/nps/id/publicapi/java/client/connection/exceptions/StompConnectionException.java
+++
@@ -1,7 +0,0 @@
-package nps.id.publicapi.java.client.connection.exceptions;
-
-public class StompConnectionException extends Exception {
-    public StompConnectionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
